bbee43c9b253d003d86d6a1df57444834e6c9845
Justin Coffey <j.coffey@criteo.com>
2015-05-27 23:58:11 +0200

    Minor clean ups
    - moved to functional model for object mappers
    - self-typing for mixins
    - bumped scalikejdbc to 2.2.6

--- src/test/scala/sqoilerplate/SQLMappableSpec.scala
+++ src/test/scala/sqoilerplate/SQLMappableSpec.scala
@@ -4,20 +4,20 @@ import org.scalatest._
 import scalikejdbc._
 import scalikejdbc.config.DBs
 
-/**
- * Created by j.coffey on 29/09/14.
- */
-class SQLMappableSpec extends FlatSpec {
-
+object DBSettings {
   // change the connection pool to our test pool
-  // TODO: test this explicitly!
   implicit val poolName = 'test
 
   DBs.setup(poolName)
+}
+
+/**
+ * Specs for SQLMappable.
+ */
+class SQLMappableSpec extends FlatSpec {
 
   // TODO: find out why this is not being pulled in from application.conf.
   GlobalSettings.loggingSQLAndTime = LoggingSQLAndTimeSettings(enabled = false)
-
 
   "A SQLMappable" should "insert data into a table" in {
     Foo.createTable
@@ -26,7 +26,7 @@ class SQLMappableSpec extends FlatSpec {
     Foo.insert(new Foo("bar"))
     assert(1 == Foo.count)
 
-    assert("bar" == Foo.fetchAll()(0).value)
+    assert("bar" == Foo.fetchAll().head.value)
   }
 
   "A Fetchable SQLMappable" should "be able to be fetched by its id" in {
@@ -46,59 +46,75 @@ class SQLMappableSpec extends FlatSpec {
     val foo = new FooWithAutoId(value = "bar")
     assert(0 == foo.id)
 
-    val fooId = FooWithAutoId.insert(foo)
-    assert(FooWithAutoId.exists(fooId.get))
-    assert("bar" == FooWithAutoId.fetch(fooId.get).get.value)
+    val fooId = FooWithAutoId.insertAndReturnId(foo)
+    assert(FooWithAutoId.exists(fooId))
+    assert("bar" == FooWithAutoId.fetch(fooId).get.value)
   }
 
-  case class Foo(value: String)
+}
 
-  object Foo extends SQLMappable[Foo] {
-    override protected val tableAliasName = "foo"
+import DBSettings._
 
-    override protected def createTableSql =
-      sqls"create table foo (value varchar(128))"
+/**
+ * For testing mapping of non-identifiable objects.
+ *
+ * @param value
+ */
+case class Foo(value: String)
 
-    override protected def model2NamedValues(from: Foo) = Seq(column.value -> from.value)
+object Foo extends SQLMappable[Foo](tableAliasName = "foo") {
+  override protected def createTableSql = sqls"create table foo (value varchar(128))"
 
-    override protected def resultSet2Model(from: WrappedResultSet) = new Foo(from.get(column.value))
+  override def model2NamedValues = { from: Foo => Seq(column.value -> from.value) }
+
+  override def resultSet2Model = { from: WrappedResultSet => new Foo(from.get(column.value)) }
+}
+
+/**
+ * For testing the insertion of identifiable and thus fetchable objects.
+ *
+ * @param id
+ * @param value
+ */
+case class FooWithId(id: Long, value: String) extends Identifiable[Long]
+
+object FooWithId extends SQLMappable[FooWithId](tableAliasName = "foo_with_id") with FetchableEntities[Long, FooWithId] {
+
+  implicit def model2NamedValues = { from: FooWithId =>
+    Seq(column.id -> from.id, column.value -> from.value)
   }
 
-  case class FooWithId(id: Long, value: String) extends Identifiable
-
-  object FooWithId extends Fetchable[FooWithId] {
-    override protected val tableAliasName: String = "foo_with_id"
-
-    override protected def model2NamedValues(from: FooWithId) = Seq(
-      column.id -> from.id,
-      column.value -> from.value
-    )
-
-    override protected def resultSet2Model(from: WrappedResultSet) = new FooWithId(
-      from.get(column.id),
-      from.get(column.value)
-    )
-
-    override protected def createTableSql =
-      sqls"create table foo_with_id (id bigint, value varchar(128))"
+  implicit def resultSet2Model = { from: WrappedResultSet =>
+    new FooWithId(from.get(column.id), from.get(column.value))
   }
 
-  case class FooWithAutoId(id: Long = 0, value: String) extends Identifiable
+  override protected def createTableSql =
+    sqls"create table foo_with_id (id bigint, value varchar(128))"
 
-  object FooWithAutoId extends Fetchable[FooWithAutoId] with AutoGeneratingIds[FooWithAutoId] {
-    override protected val tableAliasName = "foo_with_auto_id"
+}
 
-    override protected def model2NamedValues(from: FooWithAutoId) = Seq(
+/**
+ * For testing the insertion of identifiable and fetchable objects that produce their own ids.
+ *
+ * @param id
+ * @param value
+ */
+case class FooWithAutoId(id: Long = 0, value: String) extends Identifiable[Long]
+
+object FooWithAutoId extends SQLMappable[FooWithAutoId](tableAliasName = "foo_with_auto_id")
+with FetchableEntities[Long, FooWithAutoId] with AutoGeneratingIds[FooWithAutoId] {
+
+  override def model2NamedValues = { from: FooWithAutoId =>
+    Seq(
       // id column is excluded since it is auto-generated
       column.value -> from.value
     )
-
-    override protected def resultSet2Model(from: WrappedResultSet) = new FooWithAutoId(
-      from.get(column.id),
-      from.get(column.value)
-    )
-
-    override protected def createTableSql =
-      sqls"create table foo_with_auto_id (id bigint not null primary key auto_increment, value varchar(128))"
   }
+
+  override def resultSet2Model = { from: WrappedResultSet =>
+    new FooWithAutoId(from.get(column.id), from.get(column.value))
+  }
+
+  override protected def createTableSql =
+    sqls"create table foo_with_auto_id (id bigint not null primary key auto_increment, value varchar(128))"
 }
